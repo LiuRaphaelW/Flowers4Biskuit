e947502c944f8594616291a095136768b28d2aae
LiuRaphaelW <141077014+LiuRaphaelW@users.noreply.github.com>
2025-06-12 16:27:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.body.addEventListener("click", function(event) {
   console.log("You clicked at:", event.clientX, event.clientY);
-  const flower = document.createElement("div");
+  const flower = document.createElement("img");
+  flower.src = "images/flower.png"; // adjust path if needed
   flower.className = "flower";
-
-  flower.style.left = `${event.clientX - 10}px`;
-  flower.style.top = `${event.clientY - 10}px`;
+  flower.style.left = `${event.clientX - 25}px`;
+  flower.style.top = `${event.clientY - 25}px`;
   console.log("flower coord:", flower.style.left, flower.style.top);
   document.body.appendChild(flower);
 
