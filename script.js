031b31f004f56c3b2471021be1c21a91263e207f
LiuRaphaelW <141077014+LiuRaphaelW@users.noreply.github.com>
2025-06-12 17:32:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.addEventListener("click", function(event) {
   const r = Math.floor(Math.random() * 4);
   if (r === 3 ) {
   flower.src = "images/pngwing.com-2.png"; 
-  } else if (r === 3) {
+  } else if (r === 2) {
   flower.src = "images/pngwing.com-3.png"; 
   } else if (r === 1) {
   flower.src = "images/pngwing.com-4.png"; 
@@ -14,7 +14,7 @@ document.body.addEventListener("click", function(event) {
   flower.className = "flower";
   flower.style.left = `${event.clientX - 25}px`;
   flower.style.top = `${event.clientY - 25}px`;
-  console.log("flower coord:", flower.style.left, flower.style.top);
+  console.log("flower coordX:", flower.style.left, flower.style.top);
   document.body.appendChild(flower);
 
 
