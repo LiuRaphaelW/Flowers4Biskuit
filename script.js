9f58c674ee04ff69c93d7019f8dcdcd95ff0a1f6
LiuRaphaelW <141077014+LiuRaphaelW@users.noreply.github.com>
2025-06-12 17:29:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.body.addEventListener("click", function(event) {
   console.log("You clicked at:", event.clientX, event.clientY);
   const flower = document.createElement("img");
-  const n = Math.floor(Math.random() * 5);
-  if (r === 4 ) {
+  const n = Math.floor(Math.random() * 4);
+  if (r === 3 ) {
   flower.src = "images/pngwing.com-2.png"; 
   } else if (r === 3) {
   flower.src = "images/pngwing.com-3.png"; 
-  } else if (r === 2) {
+  } else if (r === 1) {
   flower.src = "images/pngwing.com-4.png"; 
   } else {
   flower.src = "images/pngwing.com.png"; 
